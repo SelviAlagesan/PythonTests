911bcc2d09c3d79d19d9d92dee4e1ed355ae314e
Selvi Alagesan <60302176+SelviAlagesan@users.noreply.github.com>
2020-02-19 20:24:32 -0500

    Random Number Generator

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
  
  1. Odd Even Test
  2. Leap Year Test
+ 3. Random Number Generator (importing Random library)
---
+++ venv/Scripts/randomInteger.py
@@ -0,0 +1,4 @@
+import random
+start = int(input("Enter a starting number: "))
+end = int(input("Enter a ending number: "))
+print(random.randint(start,end))
